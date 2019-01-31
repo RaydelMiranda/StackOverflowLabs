7d20951e8ad7e7fa4253e5d0513fe53950443505
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2019-01-31 02:41:47 -0500

    Finding absolute path for target/target.py

--- python/ast_transformer/main.py
+++ python/ast_transformer/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import ast
+import os
 from ast_transformer import AnalyserNodeTransformer
 
 # instrument_node need to be in the namespace.
 from ast_transformer import instrument_node
 
 if __name__ == "__main__":
-    with open('target/target.py', 'r') as program:
+
+    target_path = os.path.join(os.path.dirname(__file__), 'target/target.py')
+
+    with open(target_path, 'r') as program:
         # Read and parse the target script.
         tree = ast.parse(program.read())
         # Make transformations.
@@ -14,5 +18,5 @@ if __name__ == "__main__":
         # Fix locations.
         ast.fix_missing_locations(tree)
         # Compile and execute.
-        compiled = compile(tree, filename='target/target.py', mode='exec')
+        compiled = compile(tree, filename='target.py', mode='exec')
         exec(compiled)
